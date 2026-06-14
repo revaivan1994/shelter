45d11ccc6c52d69f1c5dcfd35f7ab8b5b369683c
Ivan <reva.ivan2012@gmail.com>
2026-06-15 00:36:35 +0300

    add console log

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('nice day');
+console.log(
+    `Self-check:
+Main page (70/70)
+- Markup validation: +10
+- Header logo + h1 + favicon: +5
+- Layout matches design: +35
+- CSS requirements: +15
+- Interactivity: +10
+
+Pets page (40/40)
+- Markup validation: +10
+- Header logo + h1 + favicon: +5
+- Layout matches design: +15
+- CSS requirements: +5
+- Interactivity: +10
+
+Total: 110/110`
+);
